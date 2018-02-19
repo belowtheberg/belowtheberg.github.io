b2c0a8d60b8c973650d7c98f489ef070b85df3d4
Cole Johnson <colehjohn@gmail.com>
2018-02-19 18:04:24 -0500

    Removes soundcloud player

---
+++ assets/rss.js
@@ -0,0 +1,30 @@
+const HEADERS = { 'Accept': 'application/json', 'Content-Type': 'application/json' };
+const RSS_URL = "https://api.rss2json.com/v1/api.json?rss_url=http://feeds.soundcloud.com/users/soundcloud:users:375018686/sounds.rss";
+function get(endpoint) {
+  return fetch(endpoint, {headers: HEADERS}).then((res) => res.json());
+}
+function month(m) {
+  if(m === 0) return 'Jan';
+  if(m === 1) return 'Feb';
+  if(m === 2) return 'Mar';
+  if(m === 3) return 'Apr';
+  if(m === 4) return 'May';
+  if(m === 5) return 'Jun';
+  if(m === 6) return 'Jul';
+  if(m === 7) return 'Aug';
+  if(m === 8) return 'Sep';
+  if(m === 9) return 'Oct';
+  if(m === 10) return 'Nov';
+  if(m === 11) return 'Dec';
+}
+
+get(RSS_URL).then((data) => {
+  const div = document.getElementById('episodes');
+  for(let i = 0; i < data.items.length; i++) {
+    let a = data.items[i];
+    let d = new Date(a.pubDate);
+    let html = `<div class='episode col-4'><h4>${a.title}</h4><h5>${month(d.getMonth())} ${d.getDate()}, ${d.getFullYear()}</h5><p>${a.description}</p><a href='${a.link}'><button class='episode-listen'>Listen</button></a></div>`
+    div.innerHTML += html;
+  }
+  console.log(data);
+});
