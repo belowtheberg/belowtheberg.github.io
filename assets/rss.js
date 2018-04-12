6509bea6d01593a3a32a57ca70285267f6fa91f6
Cole Johnson <colehjohn@gmail.com>
2018-04-12 16:57:32 -0400

    Add episodes route

--- assets/rss.js
+++ assets/rss.js
@@ -17,14 +17,29 @@ function month(m) {
   if(m === 10) return 'Nov';
   if(m === 11) return 'Dec';
 }
+console.log(window.location.pathname);
+if (window.location.pathname === '/') {
+  get(RSS_URL).then((data) => {
+    const div = document.getElementById('episodes');
+    for(let i = 0; i < 4; i++) {
+      let a = data.items[i];
+      let d = new Date(a.pubDate);
+      let html = `<div class='episode col-12'><h4>${a.title}</h4><h5>${month(d.getMonth())} ${d.getDate()}, ${d.getFullYear()}</h5><p>${a.description}</p><a href='${a.link}'><div class='episode-listen'>Listen</div></a></div>`
+      div.innerHTML += html;
+    }
+    console.log(data);
+  });
+}
 
-get(RSS_URL).then((data) => {
-  const div = document.getElementById('episodes');
-  for(let i = 0; i < data.items.length; i++) {
-    let a = data.items[i];
-    let d = new Date(a.pubDate);
-    let html = `<div class='episode col-12'><h4>${a.title}</h4><h5>${month(d.getMonth())} ${d.getDate()}, ${d.getFullYear()}</h5><p>${a.description}</p><a href='${a.link}'><div class='episode-listen'>Listen</div></a></div>`
-    div.innerHTML += html;
-  }
-  console.log(data);
-});
+if (window.location.pathname === '/episodes') {
+  get(RSS_URL).then((data) => {
+    const div = document.getElementById('episodes');
+    for(let i = 0; i < data.items.length; i++) {
+      let a = data.items[i];
+      let d = new Date(a.pubDate);
+      let html = `<div class='episode col-12'><h4>${a.title}</h4><h5>${month(d.getMonth())} ${d.getDate()}, ${d.getFullYear()}</h5><p>${a.description}</p><a href='${a.link}'><div class='episode-listen'>Listen</div></a></div>`
+      div.innerHTML += html;
+    }
+    console.log(data);
+  });
+}
